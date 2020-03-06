f0f87958b62b1de1eb8ccbe255cf93beb6e8c1dd
KUHAKU <shinyou1995@gmail.com>
2020-03-06 10:48:47 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,24 @@
 <html>
   <head>
     <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+    <link rel="stylesheet" href="{{mix('/css/app.css')}}">
   </head>
+
   <body>
-    @yield('content')
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">
+          <li class="nav-item" ><a class="nav-link" href="#">
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      @yield('content')
+    </div>
+
   </body>
 </html>
