2794aaca4f43dfcd6bed4addc60203eb38149baf
KUHAKU <shinyou1995@gmail.com>
2020-03-14 13:25:36 +0000

    中文的错误提示信息

---
+++ resources/lang/zh-CN/validation.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+
+
+    'custom' => [
+        'email' => [
+            'required' => '邮箱地址不能为空',
+        ],
+        'password' => [
+            '密码不能为空',
+        ]
+
+    ],
+];
