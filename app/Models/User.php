ca1c68e059eb275d1b8d7d95ab05e0e684016a61
KUHAKU <shinyou1995@gmail.com>
2020-03-27 12:30:28 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,8 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use Auth;
+
 class User extends Authenticatable
 {
     use Notifiable;
@@ -96,5 +98,14 @@ class User extends Authenticatable
     public function isFollowing($user_id)
     {
         return $this->followings()->contains($user_id);
+    }
+
+    public function feed()
+    {
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+                                ->with('user')
+                                ->orderBy('created_at', 'desc');
     }
 }
